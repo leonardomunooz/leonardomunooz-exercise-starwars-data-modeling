82db9d955263772d851b7e8921e5a633c0dcda1a
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-08-08 00:45:59 +0000

    modelado de datos star wars

--- src/models.py
+++ src/models.py
@@ -1,32 +1,79 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String,Enum, Float 
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+#     def to_dict(self):
+#         return {}
+
+
+class GenderEnum(enum.Enum):
+    masculino = 'Masculino'
+    femenino  = 'Femenino'
+
+
+class Usuario(Base):
+    __tablename__= "usuario"
+    id = Column(Integer, primary_key = True)
+    nombre_de_usuario = Column(String(15),nullable = False)
+    correo = Column(String(250),nullable = False)
+    contrasenia = Column(String(250), nullable = False)
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key = True)
+    nombre_planeta = Column(String(250), nullable = False)
+    clima = Column(String(20), nullable = True) # investigar cuantos tipos de climas hay en el universo star wars
+    creacion_planeta = Column(String(50),nullable = True)
+    poblacion  = Column(Integer, nullable = True)
+    
+
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key = True)
+    nombre_personaje = Column(String(250), nullable = False)
+    anio_nacimiento = Column(Integer, nullable = True)
+    gender = Enum(GenderEnum)  
+    estatura = Column (Float, nullable = True)
+    color_piel = Column(String(250), nullable = True)
+    color_cabello = Column(String(250), nullable = True)
+
+class Usuario_Planeta():
+    __tablename__ = 'usuarios_planeta'
+    id = Column(Integer, primary_key = True)
+    usuario_id = Column(Integer,ForeignKey('usuario.id'))
+    planeta_id = Column(Integer, ForeignKey('planeta.id'))
+
+class Usuario_Personaje():
+    __tablename__ = 'usuario_personaje'
+    id = Column(Integer, primary_key = True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    personaje_id = Column(Integer, ForeignKey('personaje.id'))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
